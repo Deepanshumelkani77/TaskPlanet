c21c75d24d91fa4ae7379dd0222bf5f91ad9f866
Deepanshumelkani77 <deepumelkani123@gmail.com>
2026-02-08 11:07:25 +0530

    fixed

--- frontend/src/pages/otherProfile/otherProfile.jsx
+++ frontend/src/pages/otherProfile/otherProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
-import './OtherProfile.css'
+import './otherProfile.css'
 
 const OtherProfile = () => {
   const { userId } = useParams()
